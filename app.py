f5c138ef71ccb91d6a75dd975833408470bb928d
omkar3311 <omkarwaghmare3311@gmail.com>
2026-01-08 07:51:35 +0530

    text extraction

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+def extract_text(file):
+    if file.type == "application/pdf":
+        text = ""
+        with pdfplumber.open(file) as pdf:
+            for page in pdf.pages:
+                text += page.extract_text() or ""
+        return text
+
+    elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        doc = Document(file)
+        return " ".join(p.text for p in doc.paragraphs)
+
+    return ""

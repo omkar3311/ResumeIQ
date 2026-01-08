bb63cc45474c69613974e50dd248b3ee7e02886b
omkar3311 <omkarwaghmare3311@gmail.com>
2026-01-08 07:50:28 +0530

    importing

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pdfplumber
+from docx import Document
+import re
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
